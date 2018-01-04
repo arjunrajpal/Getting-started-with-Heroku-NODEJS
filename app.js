1bd290e95bbfa71e453c83b4e81ab0674d084ce7
arjunrajpal <rajpal.arjun@yahoo.in>
2018-01-04 16:05:26 +0530

    code added

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express')
+
+var app = express()
+
+app.set('port', (process.env.PORT  || 5000));
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',function(req,res){
+
+	res.send("Heroku working !");
+});
+
+app.listen(app.get('port'));
+
+console.log('Server running !');
